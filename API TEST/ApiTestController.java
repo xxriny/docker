d6536ec8e061308585313721e8ccee2d8da6c3e5
xxrin <young01299@naver.com>
2023-08-09 09:50:23 +0900

    controller 추가

--- API TEST/ApiTestController.java
+++ API TEST/ApiTestController.java
@@ -1,22 +1,48 @@
 package pep.per.mint.front.controller.su;
 
-import javax.servlet.ServletContext;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonParser;
+
+import pep.per.mint.common.data.basic.ComMessage;
 import pep.per.mint.database.service.su.ApiTestService;
-
+import pep.per.mint.front.exception.ControllerException;
+import pep.per.mint.front.util.MessageSourceUtil;
 @Controller
 @RequestMapping("/su")
 @SuppressWarnings({ "rawtypes", "unchecked" })
 public class ApiTestController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ApiTestController.class);
+	@Autowired
+    private RestTemplate restTemplate;
 	
 	@Autowired
 	ApiTestService apiTestService;
@@ -26,5 +52,41 @@ public class ApiTestController {
 	
 	@Autowired
 	private ServletContext servletContext;
+	
+	@RequestMapping(method = RequestMethod.POST, value="/testUri", params="method=GET", headers="content-type=application/json")
+	public @ResponseBody ComMessage<?, ?> uriTest(
+			@RequestBody ComMessage<?, ?> comMessage,  Locale locale) throws Exception, ControllerException  {
+    RestTemplate restTemplate = new RestTemplate();
+        
+        // Request Headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        // Request Body
+        Map<String,Object> requestBody =new HashMap<>();
+        requestBody.put("member_id", "3");
+        
+        //JSONObject requestBody = new JSONObject();
+        //requestBody.put("member_id","3");
+        
+        HttpEntity<Map<String,Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+        
+        String url = "http://localhost:3000/count";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("method","GET");
 
+        // HTTP POST Request
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, requestEntity, String.class);
+        
+        
+        
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        String responseBody = responseEntity.getBody();
+
+        
+        
+        
+        System.out.println("Response Status Code: " + statusCode);
+        System.out.println("Response Body: " + responseBody);
+		return null;
+	}
 }
